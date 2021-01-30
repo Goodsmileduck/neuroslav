82b826d5564bebbc4e367833f27e8fb2986b67fa
VBMart <VBMart@list.ru>
2021-01-31 00:30:49 +0800

    Add sample db test

---
+++ app/tests/db_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+# Import modules from parent dir
+import sys
+sys.path.append('..')
+import models
+
+class TestModels(unittest.TestCase):
+    # Init
+    def setUp(self):
+        pass
+    # Clean up
+    def tearDown(self):
+        pass
+    # Each test method starts with the keyword test_
+    def test_sample(self):
+        self.assertEqual(0, 0)
+    def test_questions_exists(self):
+        self.assertGreater(models.Question.objects.all().count(), 0)
+    def test_phrases_exists(self):
+        self.assertGreater(models.Phrase.objects.all().count(), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
