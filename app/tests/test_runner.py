afcd86a3ffa308a5b33ba6deebca4afe0f5fd630
VBMart <VBMart@list.ru>
2021-02-01 03:05:12 +0800

    Add db seeder to tests

--- app/tests/test_runner.py
+++ app/tests/test_runner.py
@@ -2,6 +2,15 @@ import unittest
 import db_test
 import handler_test
 
+import seeder
+import os
+# TODO Remove seeder, data must be exists
+cwd = os.getcwd()
+nwd = cwd.replace('/tests', '')
+os.chdir(nwd)
+seeder.seed_all()
+os.chdir(cwd)
+
 appTestSuite = unittest.TestSuite()
 appTestSuite.addTest(unittest.makeSuite(db_test.TestModels))
 appTestSuite.addTest(unittest.makeSuite(handler_test.HandlerTest))
