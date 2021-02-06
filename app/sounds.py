011bdc6bf61f67025f8d6db36ca32af2cbf3b60d
cbuhotepka <67138623+cbuhotepka@users.noreply.github.com>
2021-02-06 16:10:33 +0800

    Changed sounds' fading out

--- app/sounds.py
+++ app/sounds.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
 class SoundFiles(Enum):
-    WELCOME_FIRST = 'dialogs-upload/b8fbb7ae-3100-4a8c-80ae-ad45d0295520/ede27864-e74d-48a0-8adf-9ccd32f6f462.opus'
-    WELCOME_SECOND = 'dialogs-upload/b8fbb7ae-3100-4a8c-80ae-ad45d0295520/d03f351d-34b8-4268-8e1e-293e4551e7d8.opus'
+    WELCOME_FIRST = 'dialogs-upload/b8fbb7ae-3100-4a8c-80ae-ad45d0295520/4bf52bca-90d6-4200-b37a-d198d221249b.opus'
+    WELCOME_SECOND = 'dialogs-upload/b8fbb7ae-3100-4a8c-80ae-ad45d0295520/96289bdc-266e-4c9b-9a0d-befe5f41ed54.opus'
     GREETING = 'dialogs-upload/b8fbb7ae-3100-4a8c-80ae-ad45d0295520/b0ba1662-5a7e-4cc5-b924-b386b53021a7.opus'
