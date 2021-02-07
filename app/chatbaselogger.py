69c9e5970c7818bdc972432b34e70d89bbbf2714
Serebrennikov Stanislav <goodsmileduck@gmail.com>
2021-02-07 19:39:26 +0800

    update attributes

--- app/chatbaselogger.py
+++ app/chatbaselogger.py
@@ -4,11 +4,11 @@ import logging
 
 CHATBASE_URL = 'https://chatbase-area120.appspot.com/api/message'
 
-def sendUserMessage(message, user_id, request_id):
+def sendUserMessage(message, user_id, session_id):
     try:
         if message == '':
             message = '<empty>'
-        logging.info(f'CHATBASE user message: [{message}] Session_id: {request_id}')
+        logging.info(f'CHATBASE user message: [{message}] Session_id: {session_id}')
         request = {
             "api_key": settings.CHATBASE_API_KEY,
             "type": "user",
@@ -24,16 +24,16 @@ def sendUserMessage(message, user_id, request_id):
             stream=False,
             timeout=1,
         )
-        logging.info(f'CHATBASE response: [{response.json()}] Session_id: {request_id}')
+        logging.info(f'CHATBASE response: [{response.json()}] Session_id: {session_id}')
     except Exception as e:
         logging.error(f"CHATBASE sendBotResponse EXCEPTION:{e}" )
         return None
 
-def sendBotResponse(message, user_id, request_id):
+def sendBotResponse(message, user_id, session_id):
     try:
         if message == '':
             message = '<empty>'
-        logging.info(f'CHATBASE bot response: [{message}] Session_id: {request_id}')
+        logging.info(f'CHATBASE bot response: [{message}] Session_id: {session_id}')
         request = {
             "api_key": settings.CHATBASE_API_KEY,
             "type": "agent",
@@ -49,7 +49,7 @@ def sendBotResponse(message, user_id, request_id):
             stream=False,
             timeout=1,
         )
-        logging.info(f'CHATBASE response: [{response.json()}] Session_id: {request_id}')
+        logging.info(f'CHATBASE response: [{response.json()}] Session_id: {session_id}')
     except Exception as e:
         logging.error(f"CHATBASE sendBotResponse EXCEPTION:{e}" )
         return None
